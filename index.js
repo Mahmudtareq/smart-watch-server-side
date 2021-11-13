be62f0e3a77522ebff06fcd1feb7d529f7288f23
Mahmudtareq <tareqlpu5@gmail.com>
2021-11-13 20:38:34 +0600

    Readme file chamge

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // database connection 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l1qze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+// check database 
 // console.log(uri);
 
 async function run() {
